2e82eb6809207d812500cce9dec5b1c2bf04e0c3
ParkJunHee <junhee916@naver.com>
2021-04-03 22:52:23 +0900

    2번째 중고거래 test

--- routers/exchange.js
+++ routers/exchange.js
@@ -24,7 +24,32 @@ router.get('/:exchangeId', async(req, res)=> {
 
     const exchangeDetail = await exchangeBoard.find({exchangeId: exchangeId})
 
+    if(exchangeDetail.length>0){
+        await exchangeBoard.create({images, title, price, contents})
+    }
+
     res.json({exchangeDetail: exchangeDetail})
+})
+
+//중고거래 상세 페이지 수정 
+router.patch('/:exchangeId/update', async(req, res)=> {
+    const {exchangeId} = req.params
+    const {images, title, price, contents} = req.body
+
+    const exchangeDetail = await exchangeBoard.find({exchangeId: exchangeId})
+    
+})
+
+//중고거래 상세 페이지 제거 
+router.delete('/:exchangeId/delete', async(req, res)=>{
+    const {exchangeId} = req.params
+
+    const exchangeDetail = await exchangeBoard.find({exchangeId: exchangeId})
+
+    await exchangeDetail.deleteOne({exchangeId})
+    
+    console.log("제거 성공")
+
 })
 
 
