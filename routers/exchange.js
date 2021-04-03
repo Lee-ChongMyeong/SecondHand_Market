3482e113f03c713f89de73adb97818dff491c877
ParkJunHee <junhee916@naver.com>
2021-04-03 18:12:42 +0900

    중고거래 게시판 수정

--- routers/exchange.js
+++ routers/exchange.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const exchangeBoard = require('../schemas/exchangeBoard')
 
+//중고거래 데이터 저장 
+router.post('/', async(req, res)=>{
+    const {images, title, price, contents} = req.body
+
+    await exchangeBoard.create({images, title, price, contents})
+    
+    console.log("추가 완료")
+})
+
+//중고거래 데이터 뷰
+router.get('/', async(req, res)=> {
+    const exchangeBoardData = await exchangeBoard.find({});
+
+    res.json({exchangeBoardData: exchangeBoardData})
+} )
+
+//중고거래 상세 페이지 뷰 
+router.get('/:exchangeId', async(req, res)=> {
+    const {exchangeId} = req.params
+
+    const exchangeDetail = await exchangeBoard.find({exchangeId: exchangeId})
+
+    res.json({exchangeDetail: exchangeDetail})
+})
 
 
 module.exports = router;
+
--- schemas/exchangeBoard.js
+++ schemas/exchangeBoard.js
@@ -5,20 +5,20 @@ const exchangeBoard = new Schema({
 	title: { type: String, required: true },
 	contents: { type: String, required: true },
 	price: { type: String, required: true },
-	nickname: { type: String, required: true },
+	nickname: { type: String},
 	loveCount: { type: Number, default: 0 },
-	area: { type: String, required: true },
+	area: { type: String},
 	date: { type: String, required: true, default: Date.now() },
 	soldState: { type: String},
 	images: { type: String }, // 수정 필요
 
 });
 
-user.virtual('exchangeId').get(function () {
+exchangeBoard.virtual('exchangeId').get(function () {
 	return this._id.toHexString();
 });
 
-user.set('toJSON', {
+exchangeBoard.set('toJSON', {
 	virtuals: true
 });
 
